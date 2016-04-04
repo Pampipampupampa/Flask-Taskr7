187f4f5273f759771afef451b96f9ea208839179
pampipampupampa <jeremybois@rocketmail.com>
2016-04-05 00:24:50 +0200

    Add admin user at database creation.

--- db_create.py
+++ db_create.py
@@ -1,7 +1,8 @@
 # -*- coding:Utf8 -*-
 
 
-from project import db
+from project import db, bcrypt
+from project.models import User
 
 ########################
 #    Main Program :    #
@@ -11,6 +12,12 @@ from project import db
 if __name__ == '__main__':
     # Initialize database schema (all tables, views, triggers)
     db.create_all()
+
+    # Add administrateur user
+    new_user = User(name="administrateur", email="admin@monMail.com",
+                    password=bcrypt.generate_password_hash("administrateur"),
+                    role="admin")
+    db.session.add(new_user)
 
     # Commit changes
     db.session.commit()
